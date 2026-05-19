f4d6fdca832d4f3648c124b2fb561b252f4560e8
Alexander N <1756748+lazifo@users.noreply.github.com>
2026-05-19 14:58:42 +0300

    Add data_loader.py skeleton

---
+++ modules/data_loader.py
@@ -0,0 +1,19 @@
+"""
+Module 1: DataLoader - Load DEM and route data
+"""
+
+import numpy as np
+
+class DataLoader:
+    def __init__(self):
+        pass
+    
+    def load_dem(self, bounds=None):
+        """Load Digital Elevation Model"""
+        print("[DataLoader] Loading DEM... (placeholder)")
+        return None
+    
+    def get_elevation_profile(self, route):
+        """Get elevation along route"""
+        print("[DataLoader] Getting elevation profile...")
+        return np.array([])
